92a1f841e687876c410521f18e05de46d632e56e
Claudio Jr <clauioasj@dcc.ufrj.br>
2025-12-13 17:32:10 -0300

    feat: adicao do mapa e gernalizacao dos dados

--- frontend/src/pages/mapPage.tsx
+++ frontend/src/pages/mapPage.tsx
@@ -1,4 +1,5 @@
 import { Mapa } from "@/components/mapa";
+import { AlertCircle, Leaf } from "lucide-react";
 
 export default function MapPage() {
   return (
@@ -7,7 +8,27 @@ export default function MapPage() {
         <img src="/logo.svg" alt="logo" className="h-12" />
       </header>
 
-      <div className="mt-32 max-w-5xl mx-auto p-4 max-h-32">
+      <div className="max-w-5xl p-4 mx-auto mt-32">
+        <h1 className="text-4xl font-bold">Distribuição de alimentos no estado do Rio de Janeiro</h1>
+        <h2 className="text-sm font-light mt-1">Entenda a qualidade da sua alimentação baseada no grau de processamento industrial, conforme diretrizes do Guia Alimentar para a População Brasileira.</h2>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 max-w-5xl mx-auto p-4 gap-4">
+        <div className="shadow rounded-xl border-foreground/40 p-4">
+          <div className="flex gap-1 text-red-600"> <AlertCircle /> <span>Processado</span> </div>
+          <div className="flex justify-end text-3xl font-bold">32%</div>
+        </div>
+        <div className="shadow rounded-xl border-foreground/40 p-4">
+          <div className="flex gap-1 text-green-600"> <Leaf /> <span>In Natura</span> </div>
+          <div className="flex justify-end text-3xl font-bold">8%</div>
+        </div>
+        <div className="shadow rounded-xl border-foreground/40 p-4">
+          <div className="flex gap-1 text-amber-600"> <AlertCircle /> <span>Misto</span> </div>
+          <div className="flex justify-end text-3xl font-bold">60%</div>
+        </div>
+      </div>
+
+      <div className="mt-4 max-w-5xl mx-auto p-4 overflow-hidden">
         <Mapa />
       </div>
     </>
